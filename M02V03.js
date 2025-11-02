52c46bf9f645c490f9b888320c041528d6d850d0
Your Name <you@example.com>
2025-11-02 21:16:55 +0600

    sorting and flatting array

---
+++ M02V03.js
@@ -0,0 +1,11 @@
+const fruits = ["are you", "hello", "shakhawat"];
+
+const fruitSort = fruits.sort()
+// console.log(fruitSort)
+
+
+
+
+const arr = [1, [2, 3], [4, 5]];
+const flatArr = arr.flat()
+console.log(flatArr)
